46202ec79cbebbf59d47deada71f092356f4d6b1
dpwolfe <david.p.wolfe@gmail.com>
2014-11-21 20:18:29 -0800

    Starting to construct the model view.
    Former-commit-id: bdba0dd58c32aa58abd08dc0e678e9430fa69569

--- src/terasca/ModelView.cpp
+++ src/terasca/ModelView.cpp
@@ -1,6 +1,9 @@
 #include "ModelView.h"
 
-double ModelView::mcRegionOfInterest[6] = { -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 };
+double ModelView::_mcRegionOfInterest[6] = { -1.0, 1.0, -1.0, 1.0, -1.0, 1.0 };
+s1::AffinePoint ModelView::_eye(0, 0, 2);
+s1::AffinePoint ModelView::_center(0, 0, 0);
+s1::AffineVector ModelView::_up(0, 1, 0);
 
 ModelView::ModelView()
 {
@@ -14,6 +17,13 @@ void ModelView::setMCRegionOfInterest(double xyz[6])
 {
 	for (int i = 0; i < 6; i++)
 	{
-		mcRegionOfInterest[i] = xyz[i];
+		_mcRegionOfInterest[i] = xyz[i];
 	}
+}
+
+void ModelView::setEyeCenterUp(s1::AffinePoint eye, s1::AffinePoint center, s1::AffineVector up)
+{
+	_eye = eye;
+	_center = center;
+	_up = up;
 }
--- src/terasca/ModelView.h
+++ src/terasca/ModelView.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "AffinePoint.h"
+#include "AffineVector.h"
+
 class ModelView
 {
 public:
@@ -7,7 +10,10 @@ public:
 	virtual ~ModelView();
 
 	static void setMCRegionOfInterest(double xyz[6]);
+	static void setEyeCenterUp(s1::AffinePoint eye, s1::AffinePoint center, s1::AffineVector up);
 
 protected:
-	static double mcRegionOfInterest[6];
+	static double _mcRegionOfInterest[6];
+	static s1::AffinePoint _eye, _center;
+	static s1::AffineVector _up;
 };
