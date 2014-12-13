878a991e997f653f1b9adc68e9d9df10551ec055
dpwolfe <david.p.wolfe@gmail.com>
2014-12-13 13:28:39 -0800

    Storing shader data in preparation for loading.

--- src/terasca/ShaderProgram.cpp
+++ src/terasca/ShaderProgram.cpp
@@ -1,11 +1,14 @@
 #include "ShaderProgram.h"
 
 ShaderProgram::ShaderProgram(const std::string& vertexShaderPath, const std::string& fragmentShaderPath)
+	: _shaderCount(2), _id(0), _shaders(new Shader[2], std::default_delete<Shader[]>())
 {
-
+	_shaders[0].path = vertexShaderPath;
+	_shaders[0].type = GL_VERTEX_SHADER;
+	_shaders[1].path = fragmentShaderPath;
+	_shaders[1].type = GL_FRAGMENT_SHADER;
 }
 
 ShaderProgram::~ShaderProgram()
 {
-
 }
