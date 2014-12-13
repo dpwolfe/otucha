71e67ef310779607523bc84684cd2fd6e720fcfb
dpwolfe <david.p.wolfe@gmail.com>
2014-12-13 13:28:39 -0800

    Storing shader data in preparation for loading.
    Former-commit-id: 0a5f506b95c1d255b581e26a23c0e94b12184a26

--- src/terasca/ShaderProgram.h
+++ src/terasca/ShaderProgram.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <string>
+#include <memory>
+
+#include <GL/glew.h>
+#include <GL/GL.h>
 
 class ShaderProgram
 {
@@ -11,5 +15,17 @@ public:
 	int getId() const { return _id; }
 
 private:
+	struct Shader
+	{
+		std::string path;
+		GLenum type;
+		std::string source;
+		int id;
+
+		Shader() : path(""), type(0), source(""), id(0) {}
+	};
+
 	int _id;
+	int _shaderCount;
+	std::unique_ptr<Shader[]> _shaders;
 };
