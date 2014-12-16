235df084cf4a5246b7fca59c84c2c4ce2148e06d
dpwolfe <david.p.wolfe@gmail.com>
2014-12-15 21:25:35 -0800

    Matrix4x4 copy constructor, = operator, constructor initialization.
    Former-commit-id: 42ed8b631f6e2c6e8682dcc7c9ddfba4f38eb54c

--- src/s1/Matrix4x4.h
+++ src/s1/Matrix4x4.h
@@ -6,6 +6,21 @@ namespace s1
 	{
 	public:
 		Matrix4x4();
+		Matrix4x4(const Matrix4x4& m);
+		Matrix4x4(
+			double r1c1, double r1c2, double r1c3, double r1c4,
+			double r2c1, double r2c2, double r2c3, double r2c4,
+			double r3c1, double r3c2, double r3c3, double r3c4,
+			double r4c1, double r4c2, double r4c3, double r4c4);
 		virtual ~Matrix4x4();
+
+		Matrix4x4 operator=(const Matrix4x4& rhs);
+
+		static const Matrix4x4 Identity;
+
+	private:
+		double _value[4][4];
+
+		void _copyFrom(const Matrix4x4& m);
 	};
 }
