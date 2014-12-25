48fd5f246f62448b151b7e6590c5d289ffb664fd
dpwolfe <david.p.wolfe@gmail.com>
2014-12-24 18:15:11 -0800

    Fix to inverted perspective divide.

--- src/s1/Matrix4x4.cpp
+++ src/s1/Matrix4x4.cpp
@@ -74,14 +74,14 @@ Matrix4x4 Matrix4x4::perspective(double xmin, double xmax, double ymin, double y
 	return Matrix4x4(
 		-2.0*zpp/(xmax-xmin), 0.0,                 (xmax+xmin)/(xmax-xmin),  0.0,
 		0.0,                 -2.0*zpp/(ymax-ymin), (ymax+ymin)/(ymax-ymin),  0.0,
-		0.0,                 0.0,                 -(zmax+zmin)/(zmax-zmin), 2.0*zmin*zmax/(zmax-zmin),
-		0.0,                 0.0,                 -1.0,                     0.0);
+		0.0,                  0.0,                 (zmax+zmin)/(zmax-zmin), -2.0*zmin*zmax/(zmax-zmin),
+		0.0,                  0.0,                 -1.0,                     0.0);
 }
 
 Matrix4x4 Matrix4x4::lookAt(AffinePoint& eye, AffinePoint& center, AffineVector& up)
 {
 	s1::AffineVector u, v, w;
-	w = eye - center;
+	w = center - eye;
 	w.normalize();
 	u = w.cross(up);
 	u.normalize();
@@ -90,9 +90,9 @@ Matrix4x4 Matrix4x4::lookAt(AffinePoint& eye, AffinePoint& center, AffineVector&
 	double ty = v.dot(eye);
 	double tz = w.dot(eye);
 	return Matrix4x4(
-		 u[0],  u[1],  u[2], -tx,
-		 v[0],  v[1],  v[2], -ty,
-		-w[0], -w[1], -w[2], -tz,
+		 u[0],  u[1],  u[2], tx,
+		 v[0],  v[1],  v[2], ty,
+		-w[0], -w[1], -w[2], tz,
    		  0.0,   0.0,   0.0, 1.0);
 }
 
