a7d9909d4cc8df2fa0ab0ace96f8d7d7e96dd624
dpwolfe <david.p.wolfe@gmail.com>
2015-01-04 22:15:04 -0800

    Input of additional phong values (ka, ks, m, a) Made the block material gold and put 3 lights on it for specular reflection.
    Former-commit-id: d92c491efa260957e950392053a3044faef3c5f8

--- src/terasca/ModelViewWithShader.cpp
+++ src/terasca/ModelViewWithShader.cpp
@@ -8,7 +8,11 @@ int ModelViewWithShader::_instanceCount = 0;
 GLint ModelViewWithShader::_ppuLoc_mc_ec = -2;
 GLint ModelViewWithShader::_ppuLoc_ec_dc = -2;
 GLint ModelViewWithShader::_ppuLoc_normal_mat = -2;
+GLint ModelViewWithShader::_ppuLoc_ka = -2;
 GLint ModelViewWithShader::_ppuLoc_kd = -2;
+GLint ModelViewWithShader::_ppuLoc_ks = -2;
+GLint ModelViewWithShader::_ppuLoc_m = -2;
+GLint ModelViewWithShader::_ppuLoc_a = -2;
 GLint ModelViewWithShader::_pvaLoc_mcPosition = -2;
 GLint ModelViewWithShader::_pvaLoc_mcNormal = -2;
 
@@ -43,6 +47,10 @@ void ModelViewWithShader::_initGLSLVariableLocations()
 	_ppuLoc_mc_ec = _getUniformLocation(_shaderProgramId, "mc_ec");
 	_ppuLoc_ec_dc = _getUniformLocation(_shaderProgramId, "ec_dc");
 	_ppuLoc_normal_mat = _getUniformLocation(_shaderProgramId, "normal_mat");
+	_ppuLoc_ka = _getUniformLocation(_shaderProgramId, "ka");
 	_ppuLoc_kd = _getUniformLocation(_shaderProgramId, "kd");
+	_ppuLoc_ks = _getUniformLocation(_shaderProgramId, "ks");
+	_ppuLoc_m = _getUniformLocation(_shaderProgramId, "m");
+	_ppuLoc_a = _getUniformLocation(_shaderProgramId, "a");
 	_pvaLoc_mcNormal = _getAttribLocation(_shaderProgramId, "mcNormal");
 }
