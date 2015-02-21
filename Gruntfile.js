cdc79623479001487fb08ef401f8f95d0ef1ff16
dpwolfe <david.p.wolfe@gmail.com>
2015-02-21 13:33:40 -0800

    First step toward building with grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
 module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+            shell: {
+                options: {
+                    stderr: false
+                },
+                target: {
+                    command: 'echo Use build:emscripten or build:native'
+                },
+                native: {
+                    command: [
+                        'echo Building native'
+                    ].join('&&')
+                }
+            }
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask('build', ['build:native']);
+    grunt.registerTask('build:native', ['shell:native']);
+    grunt.registerTask('default', function () {
+        grunt.log.writeln('Running "default" task.');
+        grunt.task.run(['build']);
+    });
 }
