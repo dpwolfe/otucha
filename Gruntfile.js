884ca481962e6e556e970847dac3e7630efbaaeb
dpwolfe <david.p.wolfe@gmail.com>
2015-02-21 17:12:37 -0800

    Grunt build Emscripten

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,28 @@ module.exports = function (grunt) {
                         'cd build',
                         'cmake -DOTUCHA_EMSCRIPTEN_ENABLED=OFF ../src'
                     ].join('&&')
+                },
+                emscripten_step1: {
+                    options: {
+                        stderr: false,
+                        failOnError: false
+                    },
+                    command: [
+                        'echo Building JavaScript',
+                        'cd embuild',
+                        'emcmake cmake ../src'
+                    ].join('&&')
+                },
+                emscripten_step2: {
+                    options: {
+                        stderr: false,
+                        failOnError: false
+                    },
+                    command: [
+                        'cd embuild',
+                        'emcmake cmake ../src',
+                        'make'
+                    ].join('&&')
                 }
             }
     });
@@ -28,4 +50,9 @@ module.exports = function (grunt) {
         grunt.file.mkdir('build');
         grunt.task.run(['shell:native']);
     });
+    grunt.registerTask('build:js', function () {
+        grunt.file.mkdir('embuild');
+        grunt.task.run(['shell:emscripten_step1']);
+        grunt.task.run(['shell:emscripten_step2']);
+    })
 }
