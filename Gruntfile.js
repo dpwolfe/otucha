0082b4937ec5974bfdbf707d34421cc95d6514aa
dpwolfe <david.p.wolfe@gmail.com>
2015-02-21 19:51:15 -0800

    Building Emscripten output www folder with grunt
    Former-commit-id: 2e2994e2869c5e100c3271d46cd8b19cd5b12608 [formerly c08bf465a731052eee3a5e808e12ee2a41d496c2]
    Former-commit-id: 26a9f19658ea14a55183fd6151f16080e265748d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,78 @@
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
+    grunt.loadNpmTasks('grunt-bowercopy');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-            shell: {
+        shell: {
+            options: {
+                stderr: false
+            },
+            target: {
+                command: 'echo Use build:emscripten or build:native'
+            },
+            native: {
+                command: [
+                    'echo Building native',
+                    'cd build',
+                    'cmake -DOTUCHA_EMSCRIPTEN_ENABLED=OFF ../src'
+                ].join('&&')
+            },
+            emscripten_step1: {
                 options: {
-                    stderr: false
+                    failOnError: false
                 },
-                target: {
-                    command: 'echo Use build:emscripten or build:native'
+                command: [
+                    'echo Building JavaScript',
+                    'cd embuild',
+                    'emcmake cmake ../src'
+                ].join('&&')
+            },
+            emscripten_step2: {
+                command: [
+                    'cd embuild',
+                    'emcmake cmake ../src',
+                    'make'
+                ].join('&&')
+            }
+        },
+        bowercopy: {
+            server: {
+                options: {
+                    srcPrefix: 'src',
+                    destPrefix: 'www'
                 },
-                native: {
-                    command: [
-                        'echo Building native',
-                        'cd build',
-                        'cmake -DOTUCHA_EMSCRIPTEN_ENABLED=OFF ../src'
-                    ].join('&&')
+                files: {
+                    'server.js': 'server.js'
+                }
+            },
+            app: {
+                options: {
+                    srcPrefix: 'embuild',
+                    destPrefix: 'www'
                 },
-                emscripten_step1: {
-                    options: {
-                        stderr: false,
-                        failOnError: false
-                    },
-                    command: [
-                        'echo Building JavaScript',
-                        'cd embuild',
-                        'emcmake cmake ../src'
-                    ].join('&&')
-                },
-                emscripten_step2: {
-                    options: {
-                        stderr: false,
-                        failOnError: false
-                    },
-                    command: [
-                        'cd embuild',
-                        'emcmake cmake ../src',
-                        'make'
-                    ].join('&&')
+                files: {
+                    'otucha-full.js': 'otucha.js',
+                    'index.html': 'otucha.html'
                 }
             }
+        },
+        uglify: {
+            options: {
+                sourceMap: false,
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyy-mm-dd-HH-MM-ss") %> */\n',
+                compress: true,
+                mangle: false
+            },
+            build: {
+                files: {
+                    'www/otucha.js': [
+                        'www/otucha-full.js'
+                    ]
+                }
+            }
+        }
     });
 
     grunt.registerTask('default', function () {
@@ -54,5 +87,6 @@ module.exports = function (grunt) {
         grunt.file.mkdir('embuild');
         grunt.task.run(['shell:emscripten_step1']);
         grunt.task.run(['shell:emscripten_step2']);
+        grunt.task.run(['bowercopy', 'uglify']);
     })
 }
