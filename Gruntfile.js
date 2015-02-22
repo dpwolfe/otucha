796302afcd504765371aad053d5042dfd395f8f4
dpwolfe <david.p.wolfe@gmail.com>
2015-02-21 16:08:47 -0800

    Grunt builds CMake native projects
    Former-commit-id: b9e69d8c37df55279b61d4bab3e8b5eb636202a2 [formerly 93b1130e1db8b00e49a07e24b43d66f5c1243ab7]
    Former-commit-id: 275425faf73874a93ce88a0afa029f206bf3fd7e

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,19 @@ module.exports = function (grunt) {
                 },
                 native: {
                     command: [
-                        'echo Building native'
+                        'echo Building native',
+                        'cd build',
+                        'cmake -DOTUCHA_EMSCRIPTEN_ENABLED=OFF ../src'
                     ].join('&&')
                 }
             }
     });
 
     grunt.registerTask('build', ['build:native']);
-    grunt.registerTask('build:native', ['shell:native']);
+    grunt.registerTask('build:native', function () {
+        grunt.file.mkdir('build');
+        grunt.task.run(['shell:native']);
+    });
     grunt.registerTask('default', function () {
         grunt.log.writeln('Running "default" task.');
         grunt.task.run(['build']);
