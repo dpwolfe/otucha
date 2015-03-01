2b967225d2a0e629d47d1e964ff424be532e851c
David Wolfe <david.p.wolfe@gmail.com>
2015-03-01 14:35:21 -0800

    Convert tests to test
    Former-commit-id: 2756beedf7e0dac381080e2fec664983329997b7 [formerly 8c8a77f64fd40cdf39468df3a2f667ecf19f48a2]
    Former-commit-id: e18cf10e2f8d0d86862be6e378176d8c7f86e3a0

--- src/tests/s1_Matrix3x3_tests.cpp
+++ src/tests/s1_Matrix3x3_tests.cpp
@@ -1,87 +1,87 @@
-#include "catch.hpp"
+#include "gtest/gtest.h"
 #include "Matrix3x3.hpp"
 
-TEST_CASE("Matrix3x3 equality with default constructor", "[constructors]") {
-	s1::Matrix3x3 m;
-	REQUIRE(m == m);
+TEST(Matrix3x3Constructors, Matrix3x3_equality_with_default_constructor) {
+    s1::Matrix3x3 m;
+    EXPECT_EQ(m, m);
 }
 
-TEST_CASE("Matrix3x3 default constructor is identity", "[constructors]") {
+TEST(Matrix3x3Constructors, Matrix3x3_default_constructor_is_identity) {
 	s1::Matrix3x3 m1;
 	s1::Matrix3x3 m2(1, 0, 0, 0, 1, 0, 0, 0, 1);
-	REQUIRE(m1 == m2);
+	EXPECT_EQ(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 constructor with Matrix4x4 properly takes 3x3 elements", "[constructors]") {
+TEST(Matrix3x3Constructors, Matrix3x3_constructor_with_Matrix4x4_properly_takes_3x3_elements) {
 	s1::Matrix4x4 m4x4(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 	s1::Matrix3x3 m3x3(m4x4);
 	s1::Matrix3x3 m3x3expected(0, 1, 2, 4, 5, 6, 8, 9, 10);
-	REQUIRE(m3x3 == m3x3expected);
+	EXPECT_EQ(m3x3, m3x3expected);
 }
 
-TEST_CASE("Matrix3x3 equality with explicit constructor", "[operators]") {
+TEST(Matrix3x3Operators, Matrix3x3_equality_with_explicit_constructor) {
 	s1::Matrix3x3 m(0, 1, 2, 3, 4, 5, 6, 7, 8);
-	REQUIRE(m == m);
+	EXPECT_EQ(m, m);
 }
 
-TEST_CASE("Matrix3x3 equality with copy constructor", "[operators]") {
+TEST(Matrix3x3Operators, Matrix3x3_equality_with_copy_constructor) {
 	s1::Matrix3x3 m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
 	s1::Matrix3x3 m2(m1);
-	REQUIRE(m1 == m2);
+	EXPECT_EQ(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 not equal", "[operators]") {
+TEST(Matrix3x3Operators, Matrix3x3_not_equal) {
 	s1::Matrix3x3 m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
 	s1::Matrix3x3 m2(1, 2, 3, 4, 5, 6, 7, 8, 0);
-	REQUIRE(m1 != m2);
+	EXPECT_NE(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 inverse multiplied by original equals identity", "[operators]") {
+TEST(Matrix3x3Operators, Matrix3x3_inverse_multiplied_by_original_equals_identity) {
 	s1::Matrix3x3 m1(1, 1, 2, 3, 1, 5, 6, 2, 8);
 	s1::Matrix3x3 m2(-0.5, -1, 0.75, 1.5, -1, 0.25, 0, 1, -0.5);
 	s1::Matrix3x3 m3 = m1 * m2;
-	REQUIRE(s1::Matrix3x3::Identity == m3);
+	EXPECT_EQ(s1::Matrix3x3::Identity, m3);
 }
 
-TEST_CASE("Matrix3x3 transpose", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_transpose) {
 	s1::Matrix3x3 m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
 	s1::Matrix3x3 m2 = m1.transpose();
 	s1::Matrix3x3 m3(0, 3, 6, 1, 4, 7, 2, 5, 8);
-	REQUIRE(m2 == m3);
+	EXPECT_EQ(m2, m3);
 }
 
-TEST_CASE("Matrix3x3 transpose twice", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_transpose_twice) {
 	s1::Matrix3x3 m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
 	s1::Matrix3x3 m2 = m1.transpose().transpose();
-	REQUIRE(m1 == m2);
+	EXPECT_EQ(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 inverse identity equals identity", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_inverse_identity_equals_identity) {
 	s1::Matrix3x3 m1;
 	s1::Matrix3x3 m2 = m1.inverse();
-	REQUIRE(m1 == m2);
+	EXPECT_EQ(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 inverse", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_inverse) {
 	s1::Matrix3x3 m1(1, 1, 2, 3, 1, 5, 6, 2, 8);
 	s1::Matrix3x3 m2 = m1.inverse();
 	s1::Matrix3x3 m3(-0.5, -1, 0.75, 1.5, -1, 0.25, 0, 1, -0.5);
-	REQUIRE(m2 == m3);
+	EXPECT_EQ(m2, m3);
 }
 
-TEST_CASE("Matrix3x3 inverse twice", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_inverse_twice) {
 	s1::Matrix3x3 m1(1, 1, 2, 3, 1, 5, 6, 2, 8);
 	s1::Matrix3x3 m2 = m1.inverse().inverse();
-	REQUIRE(m1 == m2);
+	EXPECT_EQ(m1, m2);
 }
 
-TEST_CASE("Matrix3x3 copy to column major", "[methods]") {
+TEST(Matrix3x3Methods, Matrix3x3_copy_to_column_major) {
 	s1::Matrix3x3 m1(0, 1, 2, 3, 4, 5, 6, 7, 8);
 	float cm[9];
 	m1.copyToColumnMajor(cm);
 	float expected[9] = { 0, 3, 6, 1, 4, 7, 2, 5, 8 };
 	for (int i = 0; i < 9; i++)
 	{
-		REQUIRE(cm[i] == expected[i]);
+		EXPECT_EQ(cm[i], expected[i]);
 	}
 }
