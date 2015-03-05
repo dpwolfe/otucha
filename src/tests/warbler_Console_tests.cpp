b07eb4034f36af239c18456c99f81eacff83941b
David Wolfe <david.p.wolfe@gmail.com>
2015-03-04 22:54:31 -0800

    Additional console tests.

--- src/tests/warbler_Console_tests.cpp
+++ src/tests/warbler_Console_tests.cpp
@@ -18,37 +18,52 @@ const std::shared_ptr<std::vector<ConsoleArgType>> ConsoleTest::args1 = std::mak
 
 TEST(constructor, default_constructor)
 {
-    ASSERT_NO_THROW(Console c);
+    Console c;
 }
 
-TEST(methods, registerCommand)
+TEST(registerCommand, valid_inputs)
 {
     Console c;
-    ASSERT_NO_THROW(c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0));
+    c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
 }
 
-TEST(methods, registerCommand_null_func_throws)
+TEST(registerCommand, null_handler_throws)
 {
     Console c;
     ASSERT_ANY_THROW(c.registerCommand("test", nullptr, ConsoleTest::args0));
 }
 
-TEST(methods, registerCommand_empty_name_throws)
+TEST(registerCommand, empty_name_throws)
 {
     Console c;
     ASSERT_ANY_THROW(c.registerCommand("", ConsoleTest::noopCommandHandler, ConsoleTest::args0));
 }
 
-TEST(methods, registerCommand_second_handler_different_signature)
+TEST(registerCommand, second_handler_different_signature)
 {
     Console c;
     c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
-    ASSERT_NO_THROW(c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args1));
+    c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args1);
 }
 
-TEST(methods, registerCommand_second_handler_same_signature_throws)
+TEST(registerCommand, second_handler_same_signature_throws)
 {
     Console c;
     c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
     ASSERT_ANY_THROW(c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0));
+}
+
+TEST(executeCommand, third_handler_same_signature_as_first_throws)
+{
+    Console c;
+    c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
+    c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args1);
+    ASSERT_ANY_THROW(c.registerCommand("test", ConsoleTest::noopCommandHandler, ConsoleTest::args0));
+}
+
+TEST(executeCommand, second_handler_same_signature_but_different_name)
+{
+    Console c;
+    c.registerCommand("test1", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
+    c.registerCommand("test2", ConsoleTest::noopCommandHandler, ConsoleTest::args0);
 }
