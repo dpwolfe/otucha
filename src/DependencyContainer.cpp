6e5071ed8e1c6609d736fc26a246c832e49d6e19
dpwolfe <david.p.wolfe@gmail.com>
2015-03-14 19:46:39 -0700

    Moved FreeType initialization to DependencyContainer

--- src/DependencyContainer.cpp
+++ src/DependencyContainer.cpp
@@ -1,5 +1,7 @@
 #include "DependencyContainer.hpp"
 
+#include <iostream>
+
 using namespace otucha;
 
 std::shared_ptr<DependencyContainer> DependencyContainer::_singleton = nullptr;
@@ -7,6 +9,7 @@ std::shared_ptr<DependencyContainer> DependencyContainer::_singleton = nullptr;
 DependencyContainer::DependencyContainer()
 {
 	_console = std::make_shared<warbler::Console>();
+	_initializeFreeType();
 }
 
 std::shared_ptr<warbler::Console> DependencyContainer::getConsole()
@@ -21,4 +24,23 @@ std::shared_ptr<DependencyContainer> DependencyContainer::getSingleton()
 		_singleton = std::make_shared<DependencyContainer>();
 	}
 	return _singleton;
+}
+
+std::shared_ptr<FT_Library> DependencyContainer::getFreeType()
+{
+	return _freeType;
+}
+
+void DependencyContainer::_initializeFreeType()
+{
+	_freeType = std::make_shared<FT_Library>();
+	FT_Error error = FT_Init_FreeType(&*_freeType);
+	if (error)
+	{
+		std::cout << "Error occurred during FreeType initialization: " << error << std::endl;
+	}
+	else
+	{
+		std::cout << "FreeType initialized" << std::endl;
+	}
 }
--- src/DependencyContainer.hpp
+++ src/DependencyContainer.hpp
@@ -2,6 +2,8 @@
 
 #include <memory>
 #include "Console.hpp"
+#include "ft2build.h"
+#include FT_FREETYPE_H
 
 namespace otucha {
 	class DependencyContainer {
@@ -9,11 +11,15 @@ namespace otucha {
 		DependencyContainer();
 		
 		std::shared_ptr<warbler::Console> getConsole();
+		std::shared_ptr<FT_Library> getFreeType();
 
 		static std::shared_ptr<DependencyContainer> getSingleton();
 
 	private:
 		std::shared_ptr<warbler::Console> _console;
+		std::shared_ptr<FT_Library> _freeType;
+
+		void _initializeFreeType();
 
 		static std::shared_ptr<DependencyContainer> _singleton;
 	};
