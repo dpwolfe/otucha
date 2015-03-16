b59340ad09034c15e61579b340cb05fdaa0034be
dpwolfe <david.p.wolfe@gmail.com>
2015-03-15 17:22:20 -0700

    Lazy loading font face in DependencyContainer

--- src/DependencyContainer.cpp
+++ src/DependencyContainer.cpp
@@ -31,6 +31,25 @@ std::shared_ptr<FT_Library> DependencyContainer::getFreeType()
 	return _freeType;
 }
 
+std::shared_ptr<FT_Face> DependencyContainer::getFontFace()
+{
+	if (_fontFace == nullptr)
+	{
+		_loadFontFace();
+	}
+	return _fontFace;
+}
+
+std::string DependencyContainer::getAppDir()
+{
+	return _appDir;
+}
+
+void DependencyContainer::setAppDir(const std::string &appDir)
+{
+	_appDir = appDir;
+}
+
 void DependencyContainer::_initializeFreeType()
 {
 	_freeType = std::make_shared<FT_Library>();
@@ -42,5 +61,22 @@ void DependencyContainer::_initializeFreeType()
 	else
 	{
 		std::cout << "FreeType initialized" << std::endl;
+	}
+}
+
+void DependencyContainer::_loadFontFace()
+{
+	if (_appDir.length() == 0) { throw new std::exception(); }
+
+	_fontFace = std::make_shared<FT_Face>();
+	auto file = _appDir + std::string("DejaVuSansMono.ttf");
+	FT_Error error = FT_New_Face(*_freeType, file.c_str(), 0, &*_fontFace);
+	if (error != FT_Err_Ok)
+	{
+		std::cout << "Error occurred loading font: " << error << std::endl;
+	}
+	else
+	{
+		std::cout << "Font loaded" << std::endl;
 	}
 }
