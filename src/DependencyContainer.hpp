a606c9244a847ed9189e66e9576996b377d55ff1
dpwolfe <david.p.wolfe@gmail.com>
2015-03-15 17:22:20 -0700

    Lazy loading font face in DependencyContainer
    Former-commit-id: 8bbedf0198907a9ffa5156c473e9c38766c53d95 [formerly ad4c03f4a7f094661c0b1be664f1db3f3e4fe8c3]
    Former-commit-id: 4c82c07cfbf787b64caefc4e2f9171f39b357540

--- src/DependencyContainer.hpp
+++ src/DependencyContainer.hpp
@@ -12,14 +12,20 @@ namespace otucha {
 		
 		std::shared_ptr<warbler::Console> getConsole();
 		std::shared_ptr<FT_Library> getFreeType();
+		std::string getAppDir();
+		void setAppDir(const std::string &appDir);
+		std::shared_ptr<FT_Face> getFontFace();
 
 		static std::shared_ptr<DependencyContainer> getSingleton();
 
 	private:
 		std::shared_ptr<warbler::Console> _console;
 		std::shared_ptr<FT_Library> _freeType;
+		std::shared_ptr<FT_Face> _fontFace;
+		std::string _appDir;
 
 		void _initializeFreeType();
+		void _loadFontFace();
 
 		static std::shared_ptr<DependencyContainer> _singleton;
 	};
