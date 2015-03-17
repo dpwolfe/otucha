757fcd4d2ead4a1892e85402765d317e997ca4c3
dpwolfe <david.p.wolfe@gmail.com>
2015-03-16 23:46:35 -0700

    Load font face in TextureFont
    Former-commit-id: abea6fdc5b656ac915ff0fe8ed47ba8f64867ec1 [formerly 05c8d4b3655166759dabbee38858c8fdad2b6d77]
    Former-commit-id: f30d1e13e39067e2b508189b0d41a1e1c9bf48fa

--- src/rffalcon/TextureFont.cpp
+++ src/rffalcon/TextureFont.cpp
@@ -1,29 +1,69 @@
 #include "TextureFont.hpp"
-#include "ft2build.h"
-#include FT_FREETYPE_H
 
 using namespace rffalcon;
+
+#define POINT_RES 64
+#define DPI 72
 
 TextureFont::TextureFont(TextureAtlas atlas, const float pointSize, const std::string &filename)
 	: _atlas(atlas), _pointSize(pointSize)
 {
-	if (!_initialize())
-	{
-		throw new std::exception();
-	}
+	_initialize(filename);
 }
 
 TextureFont::~TextureFont()
 {
 }
 
-bool TextureFont::_initialize()
+void TextureFont::_initialize(const std::string &filename)
 {
 	FT_Library library;
 	FT_Face face;
 	FT_Size_Metrics metrics;
 
+	FT_Error error = FT_Init_FreeType(&library);
+	if (error != FT_Err_Ok)
+	{
+		throw new std::exception();
+	}
 
+	_loadFace(&library, &face, filename);
+	metrics = face->size->metrics;
+	_ascender = (metrics.ascender >> 6) / 100.0;
+	_descender = (metrics.descender >> 6) / 100.0;
+	_height = (metrics.height >> 6) / 100.0;
+	_linegap = _height - _ascender + _descender;
 
-	return true;
+	FT_Done_Face(face);
+	FT_Done_FreeType(library);
+}
+
+void TextureFont::_loadFace(FT_Library *library, FT_Face *face, const std::string &filename)
+{
+	FT_Error error = FT_New_Face(*library, filename.c_str(), 0, face);
+	if (error != FT_Err_Ok)
+	{
+		throw new std::exception();
+	}
+
+	error = FT_Select_Charmap(*face, FT_ENCODING_UNICODE);
+	if (error)
+	{
+		throw new std::exception();
+	}
+
+	error = FT_Set_Char_Size(*face, (int)(_pointSize * 100.0 * POINT_RES), 0, DPI * POINT_RES, DPI);
+	if (error)
+	{
+		throw new std::exception();
+	}
+	
+	FT_Matrix matrix = {
+		(int)((1.0 / POINT_RES) * 0x10000L),
+		0,
+		0,
+		0x10000L
+	};
+
+	FT_Set_Transform(*face, &matrix, nullptr);
 }
--- src/rffalcon/TextureFont.hpp
+++ src/rffalcon/TextureFont.hpp
@@ -2,6 +2,9 @@
 
 #include "TextureAtlas.hpp"
 #include <vector>
+#include <string>
+#include "ft2build.h"
+#include FT_FREETYPE_H
 
 namespace rffalcon
 {
@@ -17,7 +20,8 @@ namespace rffalcon
 		~TextureFont();
 
 	private:
-		bool _initialize();
+		void _initialize(const std::string &filename);
+		void _loadFace(FT_Library *library, FT_Face *face, const std::string &filename);
 
 		TextureAtlas _atlas;
 		float _pointSize;
@@ -25,5 +29,6 @@ namespace rffalcon
 		float _height = 0;
 		float _ascender = 0;
 		float _descender = 0;
+		float _linegap = 0;
 	};
 }
