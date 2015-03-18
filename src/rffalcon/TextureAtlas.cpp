31cf6896d9fdcc6b4f313334752eb8bc4999e4d5
dpwolfe <david.p.wolfe@gmail.com>
2015-03-17 21:14:55 -0700

    get width, height, depth from TextureAtlas... start of TextureFont::loadGlyphs
    Former-commit-id: 910acafa99055c4df8eb35682e457744e7a3bf1e [formerly 2aaa9006d667b2e710702723ff0aafb7ee2605d3]
    Former-commit-id: c018b3dbb307f23920ea8f940b762c6e64d564ce

--- src/rffalcon/TextureAtlas.cpp
+++ src/rffalcon/TextureAtlas.cpp
@@ -16,3 +16,18 @@ TextureAtlas::TextureAtlas(const int width, const int height, const int depth) :
 TextureAtlas::~TextureAtlas()
 {
 }
+
+int TextureAtlas::getWidth() const
+{
+	return _width;
+}
+
+int TextureAtlas::getHeight() const
+{
+	return _height;
+}
+
+int TextureAtlas::getDepth() const
+{
+	return _depth;
+}
