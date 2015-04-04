24216352253a4c7c774ad5c05544717eaca92937
dpwolfe <david.p.wolfe@gmail.com>
2015-04-04 16:16:16 -0700

    Adding VertexBuffer class

---
+++ src/rffalcon/VertexBuffer.cpp
@@ -0,0 +1,13 @@
+#include "VertexBuffer.hpp"
+
+using namespace rffalcon;
+
+VertexBuffer::VertexBuffer(const std::string &format)
+{
+
+}
+
+VertexBuffer::~VertexBuffer()
+{
+
+}
---
+++ src/rffalcon/VertexBuffer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace rffalcon {
+	class VertexBuffer {
+		VertexBuffer(const std::string &format);
+		~VertexBuffer();
+	};
+}
