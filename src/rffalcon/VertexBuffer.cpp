06f93a096f4ed4641701280792574f74a0fb746a
dpwolfe <david.p.wolfe@gmail.com>
2015-04-04 23:25:08 -0700

    VertexAttribute and some prep before creating them in VertexBuffer

--- src/rffalcon/VertexBuffer.cpp
+++ src/rffalcon/VertexBuffer.cpp
@@ -1,10 +1,24 @@
 #include "VertexBuffer.hpp"
 
+#include <vector>
+#include <sstream>j
+
 using namespace rffalcon;
 
-VertexBuffer::VertexBuffer(const std::string &format)
+VertexBuffer::VertexBuffer(const std::string &format) : _format(format)
 {
+	for (int index = 0; index < MAX_VERTEX_ATTRIBUTES; ++index)
+	{
+		_attributes[index] = nullptr;
+	}
 
+	std::vector<std::string> formatParts;
+	std::stringstream formatStringStream(format);
+	std::string formatPart;
+	while (std::getline(formatStringStream, formatPart, ','))
+	{
+		formatParts.push_back(formatPart);
+	}
 }
 
 VertexBuffer::~VertexBuffer()
