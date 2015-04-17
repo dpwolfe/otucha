41362017138798c80c739c7b8a1562c0471dbd8e
dpwolfe <david.p.wolfe@gmail.com>
2015-04-16 21:54:08 -0700

    Side by side block or text rendering.
    Former-commit-id: b4b44a41f50a4c51dea972876f7ae85b40a774ea [formerly 6d66a9b335b3630fe61a3fd64928d2cc55c8575e]
    Former-commit-id: 70596ed29d3e30639f3c5982aafd4a9eb0682da7

--- src/otucha.cpp
+++ src/otucha.cpp
@@ -7,30 +7,97 @@
 #include "DependencyContainer.hpp"
 #include "otuchaConfig.h"
 #include "Matrix3x3.hpp"
+#include "TextureAtlas.hpp"
+#include "TextureFont.hpp"
+#include "vec.hpp"
+#include "VertexBuffer.hpp"
 
 #include <memory>
 #include <iostream>
 
 using namespace otucha;
 
-void set3DViewingInformation(double xyz[6])
+void set3DViewingInformation(std::shared_ptr<terasca::ModelView> modelView, double xyz[6])
 {
-	terasca::ModelView::setMCRegionOfInterest(xyz);
+	modelView->setMCRegionOfInterest(xyz);
 
 	rffalcon::AffinePoint eye(1.0, 1.0, 1.0);
 	rffalcon::AffinePoint center(0, 0, 0);
 	rffalcon::AffineVector up(0, 1, 0);
 
-	terasca::ModelView::setEyeCenterUp(eye, center, up);
+	modelView->setEyeCenterUp(eye, center, up);
 
 	double zpp = -1.0;
 	double zmin = -99.0;
 	double zmax = -0.01;
 
-	terasca::ModelView::setProjectionType(PERSPECTIVE);
-	terasca::ModelView::setZProjectionPlane(zpp);
-	terasca::ModelView::setEyeCoordinatesZMinZMax(zmin, zmax);
+	modelView->setZProjectionPlane(zpp);
+	modelView->setEyeCoordinatesZMinZMax(zmin, zmax);
 }
+
+typedef struct {
+	float x, y, z;
+	float s, t;
+	float r, g, b, a;
+} Vertex;
+
+void addText(std::shared_ptr<rffalcon::VertexBuffer> buffer, std::shared_ptr<rffalcon::TextureFont> font,
+	const std::wstring &text, rffalcon::vec2 pen, const rffalcon::vec4 color)
+{
+	int textLength = text.length();
+	for (int index = 0; index < textLength; ++index)
+	{
+		std::shared_ptr<rffalcon::TextureGlyph> glyph = font->getGlyph(text[index]);
+		if (glyph != nullptr)
+		{
+			auto glyph1 = *glyph;
+			float kerning = 0.0f;
+			if (index > 0)
+			{
+				kerning = glyph->getKerning(text[index]);
+			}
+			pen.x += kerning;
+			int x0 = static_cast<int>(pen.x + glyph->offsetX);
+			int y0 = static_cast<int>(pen.y + glyph->offsetY);
+			int x1 = static_cast<int>(x0 + glyph->width);
+			int y1 = static_cast<int>(y0 - glyph->height);
+			GLuint i[6] = { 0, 1, 2, 0, 2, 3 };
+			std::shared_ptr<std::vector<GLuint>> indices = std::make_shared<std::vector<GLuint>>(i, i+6);
+			Vertex v[4] = {
+				{ static_cast<float>(x0), static_cast<float>(y0), 0.0f, glyph->s0, glyph->t0, color.r, color.g, color.b, color.a },
+				{ static_cast<float>(x0), static_cast<float>(y1), 0.0f, glyph->s0, glyph->t1, color.r, color.g, color.b, color.a },
+				{ static_cast<float>(x1), static_cast<float>(y1), 0.0f, glyph->s1, glyph->t1, color.r, color.g, color.b, color.a },
+				{ static_cast<float>(x1), static_cast<float>(y0), 0.0f, glyph->s1, glyph->t0, color.r, color.g, color.b, color.a }
+
+			};
+			std::shared_ptr<std::vector<void*>> vertices = std::make_shared<std::vector<void*>>();
+			vertices->push_back(&v[0]);
+			vertices->push_back(&v[1]);
+			vertices->push_back(&v[2]);
+			vertices->push_back(&v[3]);
+			buffer->push(vertices, indices);
+			pen.x += glyph->advanceX;
+		}
+	}
+}
+
+std::shared_ptr<rffalcon::VertexBuffer> testText(const std::string &appDir)
+{
+	std::shared_ptr<rffalcon::TextureAtlas> atlas = std::make_shared<rffalcon::TextureAtlas>(512, 512, 1);
+	rffalcon::vec2 pen = { { 5, 400 } };
+	rffalcon::vec4 black = { { 0, 0, 0, 1 } };
+	std::shared_ptr<rffalcon::TextureFont> font = std::make_shared<rffalcon::TextureFont>(atlas, 27.0f, appDir + "DejaVuSansMono.ttf");
+	pen.x = 5;
+	pen.y -= font->getHeight();
+	std::wstring text = L"#WORKSINMAIN #WORKSINMAIN #WORKSINMAIN";
+	font->loadGlyphs(text);
+	std::shared_ptr<rffalcon::VertexBuffer> buffer = std::make_shared<rffalcon::VertexBuffer>("vertex:3f,texCoord:2f,color:4f");
+	addText(buffer, font, text, pen, black);
+	
+	glBindTexture(GL_TEXTURE_2D, atlas->getGLTextureId());
+	return buffer;
+}
+
 
 int main(int argc, char* argv[])
 {
@@ -43,15 +110,25 @@ int main(int argc, char* argv[])
 	unsigned found = appPath.find_last_of("/\\");
 	std::string appDir = appPath.substr(0, found + 1);
 	DependencyContainer::getSingleton()->setAppDir(appDir);
-	std::shared_ptr<terasca::ModelView> modelView = std::make_shared<terasca::ModelViewWithShader>(appDir + "simple.vsh", appDir + "simple.fsh");
-	std::shared_ptr<rffalcon::ModelBase> model = std::make_shared<rffalcon::Block>(-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f);
-	modelView->addModel(model);
-	c.addModelView(modelView);
+	
+	std::shared_ptr<terasca::ModelView> blockModelView = std::make_shared<terasca::ModelViewWithShader>(appDir + "simple.vsh", appDir + "simple.fsh");
+	std::shared_ptr<rffalcon::ModelBase> blockModel = std::make_shared<rffalcon::Block>(-0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f);
+	blockModelView->addModel(blockModel);
+	c.addModelView(blockModelView);
+
 
 	glClearColor(0.9f, 0.9f, 0.9f, 1.0f);
 	double xyz[6];
-	c.getOverallMCBoundingBox(xyz);
-	set3DViewingInformation(xyz);
+	blockModelView->getMCBoundingBox(xyz);
+	set3DViewingInformation(blockModelView, xyz);
+
+	std::shared_ptr<rffalcon::VertexBuffer> textModel = testText(appDir);
+	std::shared_ptr<terasca::ModelView> textModelView = std::make_shared<terasca::ModelViewWithShader>(appDir + "v3f-t2f-c4f.vsh", appDir + "v3f-t2f-c4f.fsh");
+	textModelView->addModel(textModel);
+	textModelView->setProjectionType(ORTHOGRAPHIC);
+	//c.addModelView(textModelView);
+	textModelView->getMCBoundingBox(xyz);
+	set3DViewingInformation(textModelView, xyz);
 
 	// test console code to be removed later
 	DependencyContainer::getSingleton()->getConsole()->registerCommand(L"test", [](warbler::t_consoleArgs_ptr args) {
@@ -66,3 +143,4 @@ int main(int argc, char* argv[])
 
 	return 0;
 }
+
--- src/terasca/ModelView.hpp
+++ src/terasca/ModelView.hpp
@@ -22,27 +22,30 @@ namespace terasca
 		virtual void handleCommand(unsigned char key, double ldsX, double ldsY) { }
 		virtual void getMCBoundingBox(double* xyzBounds) const;
 		void addModel(std::shared_ptr<rffalcon::ModelBase> model);
+		void setProjectionType(ProjectionType type);
 
-		static void setMCRegionOfInterest(double xyz[6]);
-		static void setEyeCenterUp(rffalcon::AffinePoint eye, rffalcon::AffinePoint center, rffalcon::AffineVector up);
-		static void setProjectionType(ProjectionType type);
-		static void setZProjectionPlane(double zpp);
-		static void setEyeCoordinatesZMinZMax(double zMin, double zMax);
+		void setMCRegionOfInterest(double xyz[6]);
+		void setEyeCenterUp(rffalcon::AffinePoint eye, rffalcon::AffinePoint center, rffalcon::AffineVector up);
+		void setZProjectionPlane(double zpp);
+		void setEyeCoordinatesZMinZMax(double zMin, double zMax);
 
 	protected:
-		static void _getMatrices(rffalcon::Matrix4x4& mc_ec, rffalcon::Matrix4x4& ec_dc);
+		ProjectionType _projectionType = PERSPECTIVE;
+
+		void _getMatrices(rffalcon::Matrix4x4& mc_ec, rffalcon::Matrix4x4& ec_dc);
+
+		double _mcRegionOfInterest[6];
+		rffalcon::AffinePoint _eye = rffalcon::AffinePoint(0, 0, 2);
+		rffalcon::AffinePoint _center = rffalcon::AffinePoint(0, 0, 0);
+		rffalcon::AffineVector _up = rffalcon::AffineVector(0, 1, 0);
+		double _zProjectionPlane = -1.0;
+		double _eyeCoordinatesZMin = -2.0;
+		double _eyeCoordinatesZMax = -0.01;
+		rffalcon::Matrix4x4 _dynamicView;
+		std::vector<std::shared_ptr<rffalcon::ModelBase>> _models;
+		
 		static GLint _getUniformLocation(GLuint programId, const std::string& name);
 		static GLint _getAttribLocation(GLuint progrmId, const std::string& name);
-
-		static double _mcRegionOfInterest[6];
-		static rffalcon::AffinePoint _eye, _center;
-		static rffalcon::AffineVector _up;
-		static ProjectionType _projectionType;
-		static double _zProjectionPlane;
-		static double _eyeCoordinatesZMin;
-		static double _eyeCoordinatesZMax;
-		static rffalcon::Matrix4x4 _dynamicView;
-		std::vector<std::shared_ptr<rffalcon::ModelBase>> _models;
 	};
 }
 
