383fbab95803204b7b53c2c8088211596f959a57
dpwolfe <david.p.wolfe@gmail.com>
2015-04-16 22:12:14 -0700

    Rendering both block and text simultaneously.

--- src/rffalcon/VertexBuffer.cpp
+++ src/rffalcon/VertexBuffer.cpp
@@ -140,7 +140,6 @@ void VertexBuffer::_renderSetup()
 		_state = CLEAN;
 	}
 
-	glClearColor(1, 1, 1, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
