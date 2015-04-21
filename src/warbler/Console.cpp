451d52bd3356ca1a6cb97d60ca8c5b703139e287
David Wolfe <david.p.wolfe@gmail.com>
2015-04-20 22:20:24 -0700

    Update Console.cpp
    Former-commit-id: 755d3ef215c01675944dd58955299c5f1636a277 [formerly a124711770067b829433bcb063869bde2290ed47]
    Former-commit-id: 7103e6cb624bd4578c6c59d37acf6cae1b6670b8

--- src/warbler/Console.cpp
+++ src/warbler/Console.cpp
@@ -143,7 +143,7 @@ t_consoleArgs_ptr Console::_getConsoleArgs(const std::wstring &input, const Cons
                 throw std::exception();
             }
         }
-        args->push_back(consoleArg);
+        args->push_back(static_cast<void*>(consoleArg));
         ++argTypeIt;
     }
     return args;
