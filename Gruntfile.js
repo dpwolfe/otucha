de66bc3a01945a1f3009fe036ecd1d9acfd35def
dpwolfe <david.p.wolfe@gmail.com>
2015-04-25 16:19:36 -0700

    1.29 base required as 1.30 of emscripten is crashing clang a lot.
    Arg, the 1.29 version needs the double-build again.
    
    
    Former-commit-id: d10206394d0762a124cba50546c384e9bd72b78d [formerly af846d2ec92d3f0fb1b3741db4768001318d619e]
    Former-commit-id: f1a8162981ca40835d10852f3f1280667ff4c562

--- Gruntfile.js
+++ Gruntfile.js
@@ -129,6 +129,7 @@ module.exports = function (grunt) {
             if (!grunt.file.exists('embuild/CMakeCache.txt')) {
                 grunt.file.mkdir('embuild');
                 grunt.task.run('shell:emscripten_cmake');
+                grunt.task.run('shell:emscripten_cmake');
             }
             grunt.task.run('shell:emscripten_make');
             grunt.task.run('bowercopy:html');
