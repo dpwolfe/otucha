14316c69e5c99ba30b46d8e7c7ab106120795dd1
dpwolfe <david.p.wolfe@gmail.com>
2015-05-08 22:45:25 -0700

    Reduce indentation on material definitions
    Former-commit-id: 028e2de8f2161c2cdac27bc0204fdde9b1404e7c [formerly 0ba2514b787c07c87f592ddfdd1b21346b46c0ce]
    Former-commit-id: edbaf6de75261637dd5ede774f67bd7c9a40cf92

--- src/rffalcon/PhongMaterial.cpp
+++ src/rffalcon/PhongMaterial.cpp
@@ -2,21 +2,27 @@
 
 using namespace rffalcon;
 
-const PhongMaterial PhongMaterial::Gold = { { 0.24725f, 0.1995f, 0.0745f },
-											{ 0.75164f, 0.60648f, 0.22648f },
-											{ 0.628281f, 0.555802f, 0.366065f },
-											51.2f,
-											1.0f };
+const PhongMaterial PhongMaterial::Gold = {
+	{ 0.24725f, 0.1995f, 0.0745f },
+	{ 0.75164f, 0.60648f, 0.22648f },
+	{ 0.628281f, 0.555802f, 0.366065f },
+	51.2f,
+	1.0f
+};
 
 
-const PhongMaterial PhongMaterial::Copper = { { 0.1912f, 0.0735f, 0.0225f },
-											  { 0.7038f, 0.27048f, 0.0825f },
-											  { 0.256777f, 0.137622f, 0.086014f },
-											  12.8f,
-											  1.0f };
+const PhongMaterial PhongMaterial::Copper = {
+	{ 0.1912f, 0.0735f, 0.0225f },
+	{ 0.7038f, 0.27048f, 0.0825f },
+	{ 0.256777f, 0.137622f, 0.086014f },
+	12.8f,
+	1.0f
+};
 
-const PhongMaterial PhongMaterial::PolishedCopper = { { 0.2295f, 0.08825f, 0.0275f },
-													  { 0.5508f, 0.2118f, 0.066f },
-													  { 0.580594f, 0.223257f, 0.0695701f },
-													  51.2f,
-													  1.0f };
+const PhongMaterial PhongMaterial::PolishedCopper = {
+	{ 0.2295f, 0.08825f, 0.0275f },
+	{ 0.5508f, 0.2118f, 0.066f },
+	{ 0.580594f, 0.223257f, 0.0695701f },
+	51.2f,
+	1.0f
+};
